d07691bc7cdd8d575fa39f29630cd56f202b7414
Sany <74278383+mipotapota@users.noreply.github.com>
2025-03-22 00:02:13 +0900

    Add files via upload

---
+++ 0314/MovieReview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.termpaper01;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView2 = (TextView) findViewById(R.id.textView2);
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar1, float rating, boolean fromUser) {
+                textView2.setText("별점 : " + rating);
+            }
+        });
+    }
+}
