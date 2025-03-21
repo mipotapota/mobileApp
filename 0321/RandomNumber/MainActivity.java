adc68ade18af40f2481d4a6df42a68372a74d85b
Sany <74278383+mipotapota@users.noreply.github.com>
2025-03-21 22:55:54 +0900

    Add files via upload

---
+++ 0321/RandomNumber/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.randomnumber;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewRandomNumber;
+    Random random = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        textViewRandomNumber = findViewById(R.id.textViewRandomNumber);
+    }
+
+    public void generateRandomNumber(View view){
+
+        int randomNumber = random.nextInt(100);
+        textViewRandomNumber.setText("난수 : " + randomNumber);
+    }
+}
