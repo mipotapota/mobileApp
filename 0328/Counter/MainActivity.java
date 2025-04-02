d55c70fb7342b1d25a2a007075e9d93a10dd6390
Sany <74278383+mipotapota@users.noreply.github.com>
2025-04-02 22:38:06 +0900

    Add files via upload

---
+++ 0328/Counter/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.counter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnAdd, btnMinus;
+    private TextView tvCount;
+    private int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvCount = findViewById(R.id.tv_count);
+        updateCountText();
+        btnAdd = findViewById(R.id.btn_add);
+        btnMinus = findViewById(R.id.btn_minus);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                updateCountText();
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count--;
+                updateCountText();
+            }
+        });
+    }
+    private void updateCountText() {
+        tvCount.setText("카운터 : " + count);
+    }
+
+}
